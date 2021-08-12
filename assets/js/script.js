fc2aa6357c11670d72beaf6a278336063d73fb25
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-08-11 19:35:19 -0700

    added functionality: Searched city name, date, and icon of current weather appended to page after search

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,9 +13,11 @@ function getCoords(city) {
         .then(function (data) {
             console.log(data);
             let newCoords = {
+                city: data.name,
                 lat: data.coord.lat,
                 lon: data.coord.lon
             };
+            console.log(newCoords);
             let searchedCoords = JSON.parse(localStorage.getItem('searchedCoords')) || [];
             searchedCoords.push(newCoords);
             localStorage.setItem('searchedCoords', JSON.stringify(searchedCoords));
@@ -26,10 +28,17 @@ function getCoords(city) {
 
 function citySearch() {
     const currCoords = JSON.parse(localStorage.getItem('searchedCoords'));
-    console.log(currCoords);
+    const h2 = document.createElement('h2');
+    const p = document.createElement('p');
+    const img = document.createElement('img');
+
+    const city = currCoords[currCoords.length - 1].city;
     const lat = currCoords[currCoords.length - 1].lat;
     const lon = currCoords[currCoords.length - 1].lon;
-    const coordSearch = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=&appid=db4bfdf57ac03972792c74c1239c237e`
+    const coordSearch = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=alerts,minutely,hourly&units=imperial&appid=db4bfdf57ac03972792c74c1239c237e`
+
+    h2.textContent = city;
+    curCityEl.appendChild(h2);
 
     fetch(coordSearch)
         .then(function (res) {
@@ -37,13 +46,26 @@ function citySearch() {
         })
         .then(function (data) {
             console.log(data);
+            const currUnix = data.current.dt;
+            const date = new Date(currUnix * 1000);
+            const month = date.getMonth() + 1;
+            const day = date.getDate();
+            const year = date.getUTCFullYear();
+
+            p.textContent = `${month}/${day}/${year}`;
+            h2.appendChild(p);
+            
+            const currWeather = data.current.weather[0].icon;
+            const weatherImg = `http://openweathermap.org/img/wn/${currWeather}@2x.png`
+
+            img.src = weatherImg;
+            p.appendChild(img);
+
         })
 };
 
 searchBtn.onclick = function () {
     let searchedCity = searchEl.value;
     getCoords(searchedCity);
-    setTimeout(citySearch, 500);
-}
-
-//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=db4bfdf57ac03972792c74c1239c237e
+    setTimeout(citySearch, 1000);
+};
