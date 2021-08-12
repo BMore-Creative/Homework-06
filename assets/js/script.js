d4fd21d900c7c54c704adfacb2e7e5a53a322589
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-08-11 18:41:14 -0700

    added functionality: searched city data is added to local storage. second, more in depth api call is done using data from first call.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,37 @@ const searchBtn = document.querySelector('#search');
 const curCityEl = document.querySelector('#currentCity');
 const fiveDayEl = document.querySelector('#fiveDay');
 
-function citySearch(city) {
-    const currentSearch = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=db4bfdf57ac03972792c74c1239c237e`
+function getCoords(city) {
+    const currSearch = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=db4bfdf57ac03972792c74c1239c237e`
 
-    fetch (currentSearch)
-        .then(function (response) {
-            return response.json();
+    fetch (currSearch)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            let newCoords = {
+                lat: data.coord.lat,
+                lon: data.coord.lon
+            };
+            let searchedCoords = JSON.parse(localStorage.getItem('searchedCoords')) || [];
+            searchedCoords.push(newCoords);
+            localStorage.setItem('searchedCoords', JSON.stringify(searchedCoords));
+        })
+};
+
+
+
+function citySearch() {
+    const currCoords = JSON.parse(localStorage.getItem('searchedCoords'));
+    console.log(currCoords);
+    const lat = currCoords[currCoords.length - 1].lat;
+    const lon = currCoords[currCoords.length - 1].lon;
+    const coordSearch = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=&appid=db4bfdf57ac03972792c74c1239c237e`
+
+    fetch(coordSearch)
+        .then(function (res) {
+            return res.json();
         })
         .then(function (data) {
             console.log(data);
@@ -17,6 +42,8 @@ function citySearch(city) {
 
 searchBtn.onclick = function () {
     let searchedCity = searchEl.value;
-    citySearch(searchedCity);
-};
+    getCoords(searchedCity);
+    setTimeout(citySearch, 500);
+}
+
 //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=db4bfdf57ac03972792c74c1239c237e
